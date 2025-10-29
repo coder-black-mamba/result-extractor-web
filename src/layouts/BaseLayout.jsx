68b4b96b81976515a42d6409c450d44094dfc178
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-10-29 18:00:41 +0600

    Speed Analytics Added

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router'
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from "@vercel/speed-insights/react"
 const BaseLayout = () => {
+  // Speed Analytics Added
   return (
     <div className='bg-gray-900'>
         <NavBar />
